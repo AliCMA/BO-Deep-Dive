ee9b8bc381be2074bcc76d5fc3db02a11fd0577b
Ali <90691339+AliCMA@users.noreply.github.com>
2023-04-22 16:47:31 +0200

    Na lang gewerkt al de OOP code veranderd, manier net als in de video.

--- G-schema/js/main.js
+++ G-schema/js/main.js
@@ -1,228 +1,204 @@
 //OOP
 class Header {
-  constructor(title, imageUrl) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-  }
+  constructor() {
+    const header = document.createElement('header');
 
-  render() {
-    const header = document.createElement("header");
-    const h1 = document.createElement("h1");
-    const img = document.createElement("img");
+    const mainTitel = document.createElement('h1');
+    mainTitel.classList.add('main_titel');
+    mainTitel.textContent = 'G-Schema';
+    header.appendChild(mainTitel);
 
-    h1.classList.add("main_titel");
-    h1.textContent = this.title;
-
-    img.setAttribute("src", this.imageUrl);
-    img.setAttribute("alt", "Logo van DOESBURG COACHING");
-
-    header.appendChild(h1);
+    const img = document.createElement('img');
+    img.src = 'img/Logo.webp';
+    img.alt = 'Logo van DOESBURG COACHING';
     header.appendChild(img);
-
-    return header;
-  }
-}
-
-class GebeurtenisnaamTabel {
-  constructor() {}
-
-  render() {
-    const gebeurtenisnaamTabel = document.createElement("div");
-    const h2 = document.createElement("h2");
-    const textarea = document.createElement("textarea");
-
-    gebeurtenisnaamTabel.classList.add("gebeurtenisnaam__tabel");
-
-    h2.classList.add("h2_gebeurtenisnaam");
-    h2.textContent = "Vul je naam hieronder in:";
-
-    textarea.classList.add("gebeurtenisnaam__typevak");
-    textarea.setAttribute("id", "gebeurtenisnaam__typevak");
-
-    gebeurtenisnaamTabel.appendChild(h2);
-    gebeurtenisnaamTabel.appendChild(textarea);
-
-    return gebeurtenisnaamTabel;
-  }
-}
-
-class GebeurtenisTabel {
-  constructor() {}
-
-  render() {
-    const gebeurtenisTabel = document.createElement("div");
-    const h2 = document.createElement("h2");
-    const textarea = document.createElement("textarea");
-    const p = document.createElement("p");
-
-    gebeurtenisTabel.classList.add("gebeurtenis__tabel");
-
-    h2.classList.add("h2_gebeurtenis");
-    h2.textContent =
-      "(A) Gebeurtenis: wie?, Met wie?, Wat?, Waar?, Wanneer?, Hoe?";
-
-    textarea.classList.add("gebeurtenis__typevak");
-    textarea.setAttribute("id", "gebeurtenis__typevak");
-
-    p.classList.add("class_p");
-    p.textContent = "ga nu eerst naar c";
-
-    gebeurtenisTabel.appendChild(h2);
-    gebeurtenisTabel.appendChild(textarea);
-    gebeurtenisTabel.appendChild(p);
-
-    return gebeurtenisTabel;
+    document.body.appendChild(header);
   }
 }
 
 class Main {
-  constructor() {}
+  constructor() {
+    const main = document.createElement('main');
 
-  render() {
-    const main = document.createElement("main");
-    const gebeurtenisnaamTabel = new GebeurtenisnaamTabel().render();
-    const gebeurtenisTabel = new GebeurtenisTabel().render();
-    const bodyContainer = document.createElement("div");
-    const tabellenWolken = document.createElement("div");
-    const mainContainer1 = document.createElement("div");
-    const wolkLinks = document.createElement("div");
-    const h1WolkLinks = document.createElement("h1");
-    const imgWolkLinks = document.createElement("img");
-    const textareaWolkLinks = document.createElement("textarea");
-    const pWolkLinks = document.createElement("p");
-    const gevoelVak1 = document.createElement("div");
-    const h2Gevoel1 = document.createElement("h2");
-    const textareaGevoelLinks = document.createElement("textarea");
-    const pGevoelLinks = document.createElement("p");
-    const gedragVak1 = document.createElement("div");
-    const h2Gedrag1 = document.createElement("h2");
-    const textareaGedragLinks = document.createElement("textarea");
-    const pGedragLinks = document.createElement("p");
-    const mainContainer2 = document.createElement("div");
-    const wolkRechts = document.createElement("div");
-    const h1WolkRechts = document.createElement("h1");
-    const imgWolkRechts = document.createElement("img");
-    const textareaWolkRechts = document.createElement("textarea");
-    const pWolkRechts = document.createElement("p");
-    const gevoelVak2 = document.createElement("div");
-    const h2Gevoel2 = document.createElement("h2");
-    const textareaGevoelRechts = document.createElement("textarea");
-    const pGevoelRechts = document.createElement("p");
-    const gedragVak2 = document.createElement("div");
-    const h2Gedrag2 = document.createElement("h2");
-    const textareaGedragRechts = document.createElement("textarea");
-    const pGedragRechts = document.createElement("p");
-
-    mainContainer1.classList.add("main__container");
-    mainContainer2.classList.add("main__container");
-    tabellenWolken.classList.add("tabellen_wolken");
-    bodyContainer.classList.add("body__container");
-    wolkLinks.classList.add("wolk_links");
-    gevoelVak1.classList.add("gevoel_vak");
-    gedragVak1.classList.add("gedrag_vak");
-    wolkRechts.classList.add("wolk_rechts");
-    gevoelVak2.classList.add("gevoel_vak");
-    gedragVak2.classList.add("gedrag_vak");
-
-    h1WolkLinks.textContent = "(B) Onhandige";
-    imgWolkLinks.setAttribute("src", "img/wolk.svg");
-    textareaWolkLinks.classList.add("wolk_links_typevak");
-    textareaWolkLinks.setAttribute("id", "wolk_links_typevak");
-    pWolkLinks.classList.add("class_p");
-    pWolkLinks.textContent = "Ga nu naar E";
-
-    h2Gevoel1.classList.add("h2_gevoel");
-    h2Gevoel1.textContent = "(C) Gevoel";
-    textareaGevoelLinks.classList.add("gevoel_links_typevak");
-    textareaGevoelLinks.setAttribute("id", "gevoel_links_typevak");
-    pGevoelLinks.classList.add("class_p");
-    pGevoelLinks.textContent = "Ga nu naar B";
-
-    h2Gedrag1.classList.add("h2_gedrag");
-    h2Gedrag1.textContent = "(C) Gedrag";
-    textareaGedragLinks.classList.add("gedrag_links_typevak");
-    textareaGedragLinks.setAttribute("id", "gedrag_links_typevak");
-    pGedragLinks.classList.add("class_p");
-    pGedragLinks.textContent = "Ga nu naar B";
-
-    h1WolkRechts.textContent = "(D) Handige";
-    imgWolkRechts.setAttribute("src", "img/wolk.svg");
-    textareaWolkRechts.classList.add("wolk_rechts_typevak");
-    textareaWolkRechts.setAttribute("id", "wolk_rechts_typevak");
-    pWolkRechts.classList.add("class_p");
-    pWolkRechts.textContent = "Klaar!";
-
-    h2Gevoel2.classList.add("h2_gevoel");
-    h2Gevoel2.textContent = "(E) Gevoel";
-    textareaGevoelRechts.classList.add("gevoel_rechts_typevak");
-    textareaGevoelRechts.setAttribute("id", "gevoel_rechts_typevak");
-    pGevoelRechts.classList.add("class_p");
-    pGevoelRechts.textContent = "Ga nu naar D";
-
-    h2Gedrag2.classList.add("h2_gedrag");
-    h2Gedrag2.textContent = "(E) Gedrag";
-    textareaGedragRechts.classList.add("gedrag_rechts_typevak");
-    textareaGedragRechts.setAttribute("id", "gedrag_rechts_typevak");
-    pGedragRechts.classList.add("class_p");
-    pGedragRechts.textContent = "Ga nu naar D";
-
-    wolkLinks.appendChild(h1WolkLinks);
-    wolkLinks.appendChild(imgWolkLinks);
-    wolkLinks.appendChild(textareaWolkLinks);
-    wolkLinks.appendChild(pWolkLinks);
-
-    gevoelVak1.appendChild(h2Gevoel1);
-    gevoelVak1.appendChild(textareaGevoelLinks);
-    gevoelVak1.appendChild(pGevoelLinks);
-
-    gedragVak1.appendChild(h2Gedrag1);
-    gedragVak1.appendChild(textareaGedragLinks);
-    gedragVak1.appendChild(pGedragLinks);
-
-    mainContainer1.appendChild(wolkLinks);
-    mainContainer1.appendChild(gevoelVak1);
-    mainContainer1.appendChild(gedragVak1);
-
-    wolkRechts.appendChild(h1WolkRechts);
-    wolkRechts.appendChild(imgWolkRechts);
-    wolkRechts.appendChild(textareaWolkRechts);
-    wolkRechts.appendChild(pWolkRechts);
-
-    gevoelVak2.appendChild(h2Gevoel2);
-    gevoelVak2.appendChild(textareaGevoelRechts);
-    gevoelVak2.appendChild(pGevoelRechts);
-
-    gedragVak2.appendChild(h2Gedrag2);
-    gedragVak2.appendChild(textareaGedragRechts);
-    gedragVak2.appendChild(pGedragRechts);
-
-    mainContainer2.appendChild(wolkRechts);
-    mainContainer2.appendChild(gevoelVak2);
-    mainContainer2.appendChild(gedragVak2);
-
-    tabellenWolken.appendChild(mainContainer1);
-    tabellenWolken.appendChild(mainContainer2);
-
-    bodyContainer.appendChild(tabellenWolken);
-
+    const gebeurtenisnaamTabel = document.createElement('div');
+    gebeurtenisnaamTabel.classList.add('gebeurtenisnaam__tabel');
     main.appendChild(gebeurtenisnaamTabel);
+
+    const h2Gebeurtenisnaam = document.createElement('h2');
+    h2Gebeurtenisnaam.classList.add('h2_gebeurtenisnaam');
+    h2Gebeurtenisnaam.textContent = 'Vul je naam hieronder in:';
+    gebeurtenisnaamTabel.appendChild(h2Gebeurtenisnaam);
+
+    const gebeurtenisnaamTypevak = document.createElement('textarea');
+    gebeurtenisnaamTypevak.classList.add('gebeurtenisnaam__typevak');
+    gebeurtenisnaamTypevak.id = 'gebeurtenisnaam__typevak';
+    gebeurtenisnaamTabel.appendChild(gebeurtenisnaamTypevak);
+
+    const gebeurtenisTabel = document.createElement('div');
+    gebeurtenisTabel.classList.add('gebeurtenis__tabel');
     main.appendChild(gebeurtenisTabel);
+
+    const h2Gebeurtenis = document.createElement('h2');
+    h2Gebeurtenis.classList.add('h2_gebeurtenis');
+    h2Gebeurtenis.innerHTML = '(A) Gebeurtenis: wie?, Met wie?, Wat?, Waar?, Wanneer?, Hoe?';
+    gebeurtenisTabel.appendChild(h2Gebeurtenis);
+
+    const gebeurtenisTypevak = document.createElement('textarea');
+    gebeurtenisTypevak.classList.add('gebeurtenis__typevak');
+    gebeurtenisTypevak.id = 'gebeurtenis__typevak';
+    gebeurtenisTabel.appendChild(gebeurtenisTypevak);
+
+    const pClassP = document.createElement('p');
+    pClassP.classList.add('class_p');
+    pClassP.textContent = 'ga nu eerst naar c';
+    gebeurtenisTabel.appendChild(pClassP);
+
+    const bodyContainer = document.createElement('div');
+    bodyContainer.classList.add('body__container');
     main.appendChild(bodyContainer);
 
-    return main;
+    const divMain = document.createElement('div');
+    bodyContainer.appendChild(divMain);
+
+    const tabellenWolken = document.createElement('div');
+    tabellenWolken.classList.add('tabellen_wolken');
+    divMain.appendChild(tabellenWolken);
+
+    const mainContainerLeft = document.createElement('div');
+    mainContainerLeft.classList.add('main__container');
+    tabellenWolken.appendChild(mainContainerLeft);
+
+    const wolkLinks = document.createElement('div');
+    wolkLinks.classList.add('wolk_links');
+    mainContainerLeft.appendChild(wolkLinks);
+
+    const h1Onhandige = document.createElement('h1');
+    h1Onhandige.textContent = '(B) Onhandige';
+    wolkLinks.appendChild(h1Onhandige);
+
+    const imgWolk = document.createElement('img');
+    imgWolk.src = 'img/wolk.svg';
+    wolkLinks.appendChild(imgWolk);
+
+    const wolkLinksTypevak = document.createElement('textarea');
+    wolkLinksTypevak.classList.add('wolk_links_typevak');
+    wolkLinksTypevak.id = 'wolk_links_typevak';
+    wolkLinks.appendChild(wolkLinksTypevak);
+
+    const pClassPB = document.createElement('p');
+    pClassPB.classList.add('class_p');
+    pClassPB.textContent = 'Ga nu naar E';
+    wolkLinks.appendChild(pClassPB);
+
+    const gevoelVak = document.createElement('div');
+    gevoelVak.classList.add('gevoel_vak');
+    mainContainerLeft.appendChild(gevoelVak);
+
+    const h2Gevoel = document.createElement('h2');
+    h2Gevoel.classList.add('h2_gevoel');
+    h2Gevoel.textContent = '(C) Gevoel';
+    gevoelVak.appendChild(h2Gevoel);
+
+    const gevoelLinksTypevak = document.createElement('textarea');
+    gevoelLinksTypevak.classList.add('gevoel_links_typevak');
+    gevoelLinksTypevak.id = 'gevoel_links_typevak';
+    gevoelVak.appendChild(gevoelLinksTypevak);
+
+    const pClassPC = document.createElement('p');
+    pClassPC.classList.add('class_p');
+    pClassPC.textContent = 'Ga nu naar B';
+    gevoelVak.appendChild(pClassPC);
+
+    const gedragVak = document.createElement('div');
+    gedragVak.classList.add('gedrag_vak');
+    mainContainerLeft.appendChild(gedragVak);
+
+    const h2Gedrag = document.createElement('h2');
+    h2Gedrag.classList.add('h2_gedrag');
+    h2Gedrag.textContent = '(C) Gedrag';
+    gedragVak.appendChild(h2Gedrag);
+
+    const gedragLinksTypevak = document.createElement('textarea');
+    gedragLinksTypevak.classList.add('gedrag_links_typevak');
+    gedragLinksTypevak.id = 'gedrag_links_typevak';
+    gedragVak.appendChild(gedragLinksTypevak);
+
+    const pClassPD = document.createElement('p');
+    pClassPD.classList.add('class_p');
+    pClassPD.textContent = 'Ga nu naar B';
+    gedragVak.appendChild(pClassPD);
+
+    const mainContainerRight = document.createElement('div');
+    mainContainerRight.classList.add('main__container');
+    tabellenWolken.appendChild(mainContainerRight);
+
+    const wolkRechts = document.createElement('div');
+    wolkRechts.classList.add('wolk_rechts');
+    mainContainerRight.appendChild(wolkRechts);
+
+    const h1Handige = document.createElement('h1');
+    h1Handige.textContent = '(D) Handige';
+    wolkRechts.appendChild(h1Handige);
+
+    const imgWolkRechts = document.createElement('img');
+    imgWolkRechts.src = 'img/wolk.svg';
+    wolkRechts.appendChild(imgWolkRechts);
+
+    const wolkRechtsTypevak = document.createElement('textarea');
+    wolkRechtsTypevak.classList.add('wolk_rechts_typevak');
+    wolkRechtsTypevak.id = 'wolk_rechts_typevak';
+    wolkRechts.appendChild(wolkRechtsTypevak);
+
+    const pClassPE = document.createElement('p');
+    pClassPE.classList.add('class_p');
+    pClassPE.textContent = 'Klaar!';
+    wolkRechts.appendChild(pClassPE);
+
+    const gevoelVakRechts = document.createElement('div');
+    gevoelVakRechts.classList.add('gevoel_vak');
+    mainContainerRight.appendChild(gevoelVakRechts);
+
+    const h2GevoelRechts = document.createElement('h2');
+    h2GevoelRechts.classList.add('h2_gevoel');
+    h2GevoelRechts.textContent = '(E) Gevoel';
+    gevoelVakRechts.appendChild(h2GevoelRechts);
+
+    const gevoelRechtsTypevak = document.createElement('textarea');
+    gevoelRechtsTypevak.classList.add('gevoel_rechts_typevak');
+    gevoelRechtsTypevak.id = 'gevoel_rechts_typevak';
+    gevoelVakRechts.appendChild(gevoelRechtsTypevak);
+
+    const pClassPF = document.createElement('p');
+    pClassPF.classList.add('class_p');
+    pClassPF.textContent = 'Ga nu naar D';
+    gevoelVakRechts.appendChild(pClassPF);
+
+    const gedragVakRechts = document.createElement('div');
+    gedragVakRechts.classList.add('gedrag_vak');
+    mainContainerRight.appendChild(gedragVakRechts);
+
+    const h2GedragRechts = document.createElement('h2');
+    h2GedragRechts.classList.add('h2_gedrag');
+    h2GedragRechts.textContent = '(E) Gedrag';
+    gedragVakRechts.appendChild(h2GedragRechts);
+
+    const gedragRechtsTypevak = document.createElement('textarea');
+    gedragRechtsTypevak.classList.add('gedrag_rechts_typevak');
+    gedragRechtsTypevak.id = 'gedrag_rechts_typevak';
+    gedragVakRechts.appendChild(gedragRechtsTypevak);
+
+    const pClassPG = document.createElement('p');
+    pClassPG.classList.add('class_p');
+    pClassPG.textContent = 'Ga nu naar D';
+    gedragVakRechts.appendChild(pClassPG);
+
+    document.body.appendChild(main);
   }
 }
 
-
-//Her bruikbare gedeelte 
-const header = new Header("G-Schema", "img/Logo.webp").render();
-const main = new Main().render();
-
-
-document.body.appendChild(header);
-document.body.appendChild(main);
-//Her bruikbare gedeelte 
-
+//Hergebruikbare code
+const header = new Header();
+const main = new Main();
 
 
 //Opslaan van data code 
